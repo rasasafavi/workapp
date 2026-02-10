407af0a72d990a347bb46ee5691f5f3250e5cbd6
gulge <gulgezapaydin@gmail.com>
2026-02-10 16:35:19 +0300

    fix warnings

--- src/pages/AnaSayfa.js
+++ src/pages/AnaSayfa.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { getWeekDates } from '../utils/weekHelpers';
 import '../styles/AnaSayfa.css';
 import { database } from '../firebase-config';
-import { ref, onValue, set, get } from 'firebase/database';
+import { ref, set, get } from 'firebase/database';
 
 function AnaSayfa() {
   const [orders, setOrders] = useState([]);
--- src/pages/BayiDetay.js
+++ src/pages/BayiDetay.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import '../styles/BayiDetay.css';
 import { database } from '../firebase-config';
 import { ref, onValue } from 'firebase/database';
